d406c8bc23894c45fd9c6e87748c00154d54aa69
Peter Allin <peter@peca.dk>
2015-01-06 21:33:38 +0100

    Configure the alt keys on mac

--- settings.el
+++ settings.el
@@ -4,6 +4,11 @@
 (helm-mode)
 (set 'ring-bell-function 'ignore)
 
+; Mac config
+(set 'mac-right-option-modifier 'meta)
+(set 'mac-option-modifier 'none)
+
+
 ; Do nothing when user presses C-z under a window system
 (when (window-system)
   (global-set-key (kbd "C-z") (lambda () (interactive)))
